e3dc256b2e586e6f7216a87ee4151439b7594f43
kenll99minecart <kenll@hotmail.com.hk>
2021-08-01 17:03:35 +0800

    Added OmpR, quiver function accounts for vector plot

---
+++ OmpR.m
@@ -0,0 +1,119 @@
+function ans = OmpR(minR,maxR)
+%SS for OmpR versus 
+
+KC=20e-3;
+KF=1e-3;
+KF4=20e-3;
+kG=1;
+kR=1;
+kC=0.01;
+kF=0.01;
+kdG=0.001;
+kdR=0.001;
+kdGm=1;
+kdRm=1;
+KdG=1e-3;
+KdR=1e-3;
+OmpRP=linspace(minR,maxR,1000);
+% ansNorm=zeros(2,100);
+% iteration=1;
+% ops=optimoptions(@fsolve,'FunctionTolerance',1e-20);
+% for i = 1:100
+% ansNorm(:,2)=fsolve(@(G,R) fnorm(G,R),100,100,ops);
+% iteration=iteration+1;
+% end
+
+%ansAnti=fsovle(@(x) fanti(x),initial);
+
+GFP=kG.*kC.*OmpRP.^2./(OmpRP.^2+KC.^2)./(kdG.*kdGm);
+RFP=kR.*kF.*OmpRP.^2./(OmpRP.^2+KF.^2).*(1-(OmpRP.^2./(OmpRP.^2+KF4.^2)))./(kdR.*kdRm);
+
+GFPm=kC.*OmpRP.^2./(OmpRP.^2+KC.^2)./kdGm;
+RFPm=kF.*OmpRP.^2./(OmpRP.^2+KF.^2).*(1-(OmpRP.^2./(OmpRP.^2+KF4.^2)))./kdRm;
+antiG=RFPm;
+antiR=GFPm;
+leftGFP=GFPm.*KdG./(KdG+antiG);
+leftRFP=RFPm.*KdR./(KdR+antiR);
+
+GFPanti=kG.*leftGFP./kdG;
+RFPanti=kR.*leftRFP./kdR;
+%assume fast binding;
+%0=kC.*OmpRP.^2./(OmpRP.^2+KC.^2)-kdGm.*GFPm-kb.*GFPm.*antiG+kf.*;
+ans=fzero(@(OmpRPi) kF.*OmpRPi.^2./(OmpRPi.^2+KF.^2).*(1-(OmpRPi.^2./(OmpRPi.^2+KF4.^2)))./kdRm-kC.*OmpRPi.^2./(OmpRPi.^2+KC.^2)./kdGm,0.02)
+
+figure(1);
+xlabel('OmpRP / \muM');
+ylabel('FPs / \muM');
+plot(OmpRP,GFP,'g-',OmpRP,RFP,'r-',OmpRP,GFPanti,'b-',OmpRP,RFPanti,'c-');
+legend('GFP','RFP','GFP with antisense','RFP with antisense')
+
+figure(2);
+plot(RFP,GFP,'Color',[0 1 0]);
+xlabel('RFP');
+ylabel('GFP')
+
+
+figure(3);
+plot(RFPanti,GFPanti,'b-');
+xlabel('RFP');
+ylabel('GFP')
+
+%F(max(length(OmpRP))) = struct('cdata',[],'colormap',[]);
+cap=figure(4);
+
+%vid=VideoWriter('v2.mp4','MPEG-4');
+%vid.FrameRate=60;
+%open(vid)
+Vt=ones(15,15);
+Ut=ones(15,15);
+for i=1:max(length(OmpRP))
+x=linspace(0,30,15);
+y=linspace(0,30,15);
+[X,Y]=meshgrid(x,y);
+grid on;
+V=kG.*kC.*OmpRP(i).^2./(OmpRP(i).^2+KC.^2)-kdG.*Y;
+U=kR.*kF.*OmpRP(i).^2./(OmpRP(i).^2+KF.^2).*(1-(OmpRP(i).^2./(OmpRP(i).^2+KF4.^2)))-kdR.*X;
+Vt=Vt+V;
+Ut=Ut+U;
+GFPi=kG.*kC.*OmpRP(i).^2./(OmpRP(i).^2+KC.^2)./(kdG.*kdGm);
+RFPi=kR.*kF.*OmpRP(i).^2./(OmpRP(i).^2+KF.^2).*(1-(OmpRP(i).^2./(OmpRP(i).^2+KF4.^2)))./(kdR.*kdRm);
+
+quiver(X,Y,U,V,'r','LineWidth',0.9);
+axis equal
+hold on;
+plot(RFP,GFP,'b-',RFPi,GFPi,'ro');
+xlabel('RFP');
+ylabel('GFP');
+%drawnow limitrate
+%F(i) = getframe(gcf);
+hold off;
+%writeVideo(vid,F(i));
+end
+
+%close(vid);
+%movie(F);
+
+quiver(X,Y,Ut,Vt,'m');
+hold on;
+plot(RFP,GFP,'b-');
+xlabel('RFP');
+ylabel('GFP');
+
+figure(5);
+i=length(OmpRP)/2;
+Vc=kG.*kC.*OmpRP(i).^2./(OmpRP(i).^2+KC.^2)-kdG.*Y;
+Uc=kR.*kF.*OmpRP(i).^2./(OmpRP(i).^2+KF.^2).*(1-(OmpRP(i).^2./(OmpRP(i).^2+KF4.^2)))-kdR.*X;
+quiver(X,Y,Uc,zeros(15,15))
+hold on;
+quiver(X,Y,zeros(15,15),Vc);
+
+
+    function dxdt=fv(t,x)
+        GFPf=x(1);
+        RFPf=x(2);
+        dGFPdt=kG.*kC.*OmpRP.^2./(OmpRP.^2+KC.^2)-kdG.*GFPf;
+        dRFPdt=kR.*kF.*OmpRP.^2./(OmpRP.^2+KF.^2).*(1-(OmpRP.^2./(OmpRP.^2+KF4.^2)))-kdR.*RFPf;
+    dxdt=[dGFPdt;dRFPdt];
+    end
+end
+
